87a7348d4d331362eb203f759fb99617d065b406
Prabhat-6701 <kumarprabhu6701123@gmail.com>
2025-01-09 20:50:22 +0530

    added

--- Linkedlist/detectCycle.cpp
+++ Linkedlist/detectCycle.cpp
@@ -1,3 +1,63 @@
+//brute
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+
+//     Node(int val){
+//         data = val;
+//         next = NULL;
+//     }
+// };
+
+// bool hasCycle(Node* head){
+//     Node* temp = head;
+
+//     unordered_map<Node*, int>mpp;
+
+//     while(temp){
+//         if(mpp.find(temp) != mpp.end()){
+//             return true;
+//         }
+//         mpp[temp] = 1;
+//         temp = temp->next;
+//     }
+//     return false;
+// }
+
+// int main(){
+//     Node* head = new Node(1);
+//     Node* second = new Node(2);
+//     Node* third = new Node(3);
+//     Node* fourth = new Node(4);
+//     Node* fifth = new Node(5);
+
+//     head->next = second;
+//     second->next = third;
+//     third->next = fourth;
+//     fourth->next = fifth;
+//      // Create a loop
+//     fifth->next = third; 
+
+//     if(hasCycle(head)){
+//         cout<<"cycle is present"<<endl;
+//     }
+//     else{
+//         cout<<"cycle is not present"<<endl;
+//     }
+//     delete head;
+//     delete second;
+//     delete third;
+//     delete fourth;
+//     delete fifth;
+
+    
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -13,18 +73,18 @@ class Node{
 };
 
 bool hasCycle(Node* head){
-    Node* temp = head;
+    Node* slow = head;
+        Node* fast = head;
 
-    unordered_map<Node*, int>mpp;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
 
-    while(temp){
-        if(mpp.find(temp) != mpp.end()){
-            return true;
+            if(slow == fast){
+                return true;
+            }
         }
-        mpp[temp] = 1;
-        temp = temp->next;
-    }
-    return false;
+        return false;
 }
 
 int main(){
