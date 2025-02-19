d8171d77ab7e46ba788169098c50065aeb831ac5
Prabhat-6701 <kumarprabhu6701123@gmail.com>
2025-02-19 12:21:52 +0530

    print dups

---
+++ A2Z/strings/printAlldups.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printDups(string s){
+    unordered_map<char,int>mpp;
+
+    for(int i = 0; i<s.length(); i++){
+        mpp[s[i]]++;
+    }
+
+    for(auto it : mpp){
+        if(it.second > 1){
+            cout<<it.first<<"->"<<it.second<<endl;
+        }
+      
+    }
+}
+
+int main(){
+    string s;
+    cout<<"Enter the string"<<endl;
+    cin>>s;
+
+    printDups(s);
+}
